667d46970cc6da7e5d88e4ba7b6ff22f2c9c166e
Edgar Quansah <quansahedgar@gmail.com>
2022-05-16 21:52:57 -0400

    added content Dell to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+Dell Technologies creates technology solutions for a changing world. Our IT Architecture team translates our customers’ business requirements into total enterprise-wide solutions. It takes deep technical knowledge to create architectural designs that exceed service requirements. We’re skilled in analyzing the customer’s business goals, objectives, needs and general business environment to create technical system solutions. Our expertise includes integrating hardware, processes, methodologies and software into the customer environment.
+
+Join us as a Senior Horizon Presentation Engineer on our IT Architecture team in a US Remote Location to do the best work of your career and make a profound social impact.
+
+What You’ll Achieve
+
+As a Senior Horizon Presentation Engineer you will maintain the current and future Horizon Apps Healthcare tenant environments. You will be responsible for ensuring that all environments are up and operational 24/7.
+
+You will:
+Maintain and operate in daily operational support for Healthcare. It includes but not limited to monitoring, performance and capacity metrics, and SLAs.
+Expert knowledge in development/PowerShell scripting.
+Expert in troubleshooting issues that relate to the Citrix or Horizon Apps platform.
+Respond to emergency and after-hours issues.
+
+Take the first step towards your dream career
+
+Every Dell Technologies team member brings something unique to the table. Here’s what we are looking for with this role:
+
+Essential Requirements
+5 + years’ experience working with Horizon Apps.
+5 + years’ experience working with Microsoft Windows Server Operating Systems.
+Current VMware Certified Professional – Desktop and Mobility Certification(s).
+
+Desirable Requirements
+Experience working within an Epic Healthcare environment.
+
+Here’s our story; now tell us yours
+
+Dell Technologies helps organizations and individuals build a brighter digital tomorrow. Our company is made up of more than 150,000 people, located in over 180 locations around the world. We’re proud to be a diverse and inclusive team and have an endless passion for our mission to drive human progress.
+
+What’s most important to us is that you are respected, feel like you can be yourself and have the opportunity to do the best work of your life -- while still having a life. We offer excellent benefits, bonus programs, flexible work arrangements, a variety of career development opportunities, employee resource groups, and much more.
+
+We started with computers, but we didn’t stop there. We are helping customers move into the future with multi-cloud, AI and machine learning through the most innovative technology and services portfolio for the data era. Join us and become a part of what’s next in technology, starting today.
+
+You can also learn more about us by reading our latest Diversity and Inclusion Report and our plan to make the world a better place by 2030 here.
+
+Dell is committed to the principle of equal employment opportunity for all employees and to providing employees with a work environment free of discrimination and harassment. All employment decisions at Dell are based on business needs, job requirements and individual qualifications, without regard to race, color, religion or belief, national, social or ethnic origin, sex (including pregnancy), age, physical, mental or sensory disability, HIV Status, sexual orientation, gender identity and/or expression, marital, civil union or domestic partnership status, past or present military service, family medical history or genetic information, family or parental status, or any other status protected by the laws or regulations in the locations where we operate. Dell will not tolerate discrimination or harassment based on any of these characteristics. Dell encourages applicants of all age
